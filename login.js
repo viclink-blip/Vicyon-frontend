3dc910b2198d065da4eac88dff16d12026a041bc
viclink-blip <masilavincent32@gmail.com>
2026-01-09 09:03:47 +0300

    Update login API endpoint to production URL

--- login.js
+++ login.js
@@ -9,7 +9,7 @@ document.getElementById("loginForm").addEventListener("submit", async (e) => {
   message.className = "";
 
   try {
-    const res = await fetch("http://127.0.0.1:5000/login", {
+    const res = await fetch("http://vicyon.onrender.com/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
