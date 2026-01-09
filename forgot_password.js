be27a09e4112f1cf72a100097815c770fc4ce54e
viclink-blip <masilavincent32@gmail.com>
2026-01-09 09:00:34 +0300

    Update fetch URL for forgot password endpoint

--- forgot_password.js
+++ forgot_password.js
@@ -8,7 +8,7 @@ document.getElementById("forgotForm").addEventListener("submit", async (e) => {
   message.textContent = "Processing...";
 
   try {
-    const response = await fetch("http://127.0.0.1:5000/forgot-password", {
+    const response = await fetch("http://vicyon.onrender.com/forgot-password", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
